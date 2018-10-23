e7fb850397081da0ee3f48d242507c9a0d486c92
Gary Baldwin <g11baldwin@gmail.com>
2018-10-23 07:57:24 -0400

    Added NSUserDefault flag to detect re-install

--- PlayPortal/PPManager.swift
+++ PlayPortal/PPManager.swift
@@ -30,6 +30,7 @@ class PPManager {
     private var cid:String = ""
     private var cse:String
     private var redir:String
+    private var isNewBoot = false
     
     var userListener:(_ user: PPUserObject?, _ isAuthd: Bool) -> Void
     
@@ -39,6 +40,7 @@ class PPManager {
     
     //    var keychain: KeychainWrapper = KeychainWrapper()
     let keychain = KeychainSwift()
+    
     
     public init() {
         env = "SANDBOX"
@@ -50,7 +52,6 @@ class PPManager {
         //    keychain = KeychainWrapper()
         PPusersvc = PPUserService(keychain:keychain)
         PPdatasvc = PPDataService()
-        
         
         if keychain.get("apns_device_token") != nil { } else {apnsDeviceToken = ""}
         if keychain.get("refresh_token") != nil { } else {refreshToken = ""}
@@ -67,7 +68,7 @@ class PPManager {
         return instance
     }()
     
-    
+    func newInstall() -> Void { PPManager.sharedInstance.isNewBoot = true }
     func getApiUrlBase() -> String { return PPManager.sharedInstance.apiUrlBase }
     func getClientId() -> String { return PPManager.sharedInstance.cid }
     func getRedirectURI() -> String { return PPManager.sharedInstance.redir }
@@ -103,6 +104,14 @@ class PPManager {
             //            keychain = KeychainWrapper()
             //            keychain = KeychainSwift()
             PPusersvc = PPUserService(keychain:keychain)
+            
+            if(isNewBoot) {
+                print("new boot is true")
+                keychain.set("", forKey: "refresh_token")
+                keychain.set("", forKey: "access_token")
+                isNewBoot = false
+            }
+            
             PPdatasvc = PPDataService()
             
             if let a = keychain.get("apns_device_token") {
