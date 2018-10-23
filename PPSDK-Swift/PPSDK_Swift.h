eed385cf2cf87aa91aabc2406171f61466b4d768
Lincoln Fraley <lincoln@dynepic.com>
2018-10-23 07:57:14 -0400

    Initial Commit

---
+++ PPSDK-Swift/PPSDK_Swift.h
@@ -0,0 +1,19 @@
+//
+//  PPSDK_Swift.h
+//  PPSDK-Swift
+//
+//  Created by Lincoln Fraley on 10/23/18.
+//  Copyright © 2018 Lincoln Fraley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PPSDK_Swift.
+FOUNDATION_EXPORT double PPSDK_SwiftVersionNumber;
+
+//! Project version string for PPSDK_Swift.
+FOUNDATION_EXPORT const unsigned char PPSDK_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPSDK_Swift/PublicHeader.h>
+
+
