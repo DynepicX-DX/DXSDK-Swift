3560c85ae095cb9ff1bdb87db4261c40ed4caf73
Lincoln Fraley <lincoln@dynepic.com>
2018-10-24 13:46:45 -0400

    added sso button image

--- Source/RequestHandler.swift
+++ Source/RequestHandler.swift
@@ -9,3 +9,13 @@ import Foundation
 import Alamofire
 
 //  Responsible for making requests to playPORTAL api services and handling token refresh
+internal class RequestHandler {
+    
+    private init() {}
+    
+    internal static let shared: SessionManager = {
+        
+    }
+}
+
+//  Adapt
