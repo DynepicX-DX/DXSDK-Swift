c6967f694efa9412dcfc97aabce142844edf9083
Lincoln Fraley <lincoln.fraley93@gmail.com>
2020-07-27 16:36:21 -0400

    added new course and lesson routes

--- Source/PlayPortalLesson.swift
+++ Source/PlayPortalLesson.swift
@@ -8,34 +8,42 @@
 import Foundation
 
 public struct PlayPortalLesson: Codable {
-  
-  public let lessonId: String
-  public var courseId: String?
-  public let name: String
-  public var description: String?
-  public var profilePic: String?
-  public var coverPhoto: String?
-  public var media: String?
-  public let childLessons: [PlayPortalLesson]
-  public var expectedCompletionTime: Int?
-  public var instructorGraded: Bool?
-  public var `public`: Bool?
+    
+    public let lessonId: String
+    public var courseId: String?
+    public let name: String
+    public var description: String?
+    public var profilePic: String?
+    public var coverPhoto: String?
+    public var media: String?
+    public let childLessons: [PlayPortalLesson]
+    public var expectedCompletionTime: Int?
+    public var instructorGraded: Bool?
+    public var `public`: Bool?
+    public let locked: Bool
 }
 
 public struct PlayPortalLessonProgress: Codable {
-  
-  public let studentId: String
-  public let lessonId: String
-  public let pass: Bool
-  public var score: Int?
-  public let answers: [PlayPortalAssessmentAnswer]
-  public var started: String?
-  public var completed: String?
+    
+    public let studentId: String
+    public let lessonId: String
+    public let pass: Bool
+    public var score: Int?
+    public let answers: [PlayPortalAssessmentAnswer]
+    public var started: String?
+    public var completed: String?
+    public let progressIntervals: [Interval]
+    public let idleIntervals: [Interval]
+    
+    public class Interval: Codable {
+        public let start: String
+        public let end: String?
+    }
 }
 
 public struct PlayPortalAssessmentAnswer: Codable {
-  
-  public let question: String
-  public var answer: String?
-  public var correct: Bool?
+    
+    public let question: String
+    public var answer: String?
+    public var correct: Bool?
 }
